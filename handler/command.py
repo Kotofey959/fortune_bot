00d5419f1202cdf4e445a50f158e21e7aa5eb523
Aleksandr Eskov <mullikaloll@yandex.ru>
2023-12-26 22:21:05 +0500

    refact

--- handler/command.py
+++ handler/command.py
@@ -7,12 +7,11 @@ from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
-from button.user import SPIN, START_CHAT, ROULETTE
+from button.user import START_CHAT, ROULETTE
 from helper.string import get_ref_id
 from keyboard.user import create_inline
 from main import LOGGER
 from model.user import UserModel
-from roulette.prizes import FILE_IDS
 from text import MENU_START_TEXT, NEW_REFERRAL
 
 command_router = Router()
@@ -33,7 +32,6 @@ async def start(message: Message, state: FSMContext, bot: Bot):
     if not new_user_obj.record:
         await new_user_obj.create(bot)
         ref_id = get_ref_id(message.text)
-        LOGGER.warn(f"АЛАРМ {ref_id}")
         user_obj = UserModel(ref_id)
         user_obj.change_spin_count(1)
         answer_text = NEW_REFERRAL.format(user_obj.available_spins)
